c83b10e10c938d1e8cb1da274e9975272eb5d2e5
Philippe Antoine <contact@catenacyber.fr>
2021-03-15 12:08:35 +0100

    fixup target

--- projects/netcdf/build.sh
+++ projects/netcdf/build.sh
@@ -16,6 +16,7 @@
 ################################################################################
 
 # build project
+git apply ../patch.diff
 mkdir build
 cd build
 cmake -DBUILD_SHARED_LIBS=OFF ..

cd3cf730f611a56c58072fddb92aac67b46cf721
Philippe Antoine <contact@catenacyber.fr>
2021-04-07 16:03:06 +0200

    fixu

--- projects/go-pprof/go-lpm.cc
+++ projects/go-pprof/go-lpm.cc
@@ -7,7 +7,7 @@ DEFINE_PROTO_FUZZER(const perftools::profiles::Profile& input) {
     size_t size = input.ByteSizeLong();
     if (size > 0) {
         uint8_t *buffer = (uint8_t *) malloc(size);
-        printf("lol %d: %s\n", size, input.DebugString());
+        printf("lol %d: %s\n", size, input.DebugString().c_str());
         input.SerializeToArray((uint8_t *) buffer, size);
         LPMFuzzerTestOneInput(buffer, size);
         free(buffer);

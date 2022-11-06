9ae92a8c336bc6f7105d10ab38df324209138f1d
Philippe Antoine <contact@catenacyber.fr>
2022-11-06 21:45:44 +0100

    example

--- projects/openssl/build.sh
+++ projects/openssl/build.sh
@@ -35,3 +35,16 @@ done
 cp $SRC/*.options $OUT/
 cp fuzz/oids.txt $OUT/asn1.dict
 cp fuzz/oids.txt $OUT/x509.dict
+
+(
+cd $SRC/aware
+mkdir genfiles
+# TODO not sure proto_path or -I
+$SRC/LPM/external.protobuf/bin/protoc $SRC/aware/DaSSLFuzzInputToServer.proto --cpp_out=genfiles --proto_path=$SRC/aware/
+$CXX $CXXFLAGS -c -I genfiles/ -I $SRC/LPM/external.protobuf/include genfiles/DaSSLFuzzInputToServer.pb.cc
+$CXX $CXXFLAGS -c -I. -I $SRC/libprotobuf-mutator/ -I $SRC/LPM/external.protobuf/include serializer.cc
+$CXX $CXXFLAGS -c -I. -I $SRC/libprotobuf-mutator/ -I $SRC/LPM/external.protobuf/include toServer.cc
+
+# TODO check openssl path to static lib + other libs needed (libcrypto.a ?)
+$CXX $CXXFLAGS $LIB_FUZZING_ENGINE DaSSLFuzzInputToServer.pb.o serializer.o toServer.o $SRC/openssl/libssl.a $SRC/LPM/src/libfuzzer/libprotobuf-mutator-libfuzzer.a $SRC/LPM/src/libprotobuf-mutator.a $SRC/LPM/external.protobuf/lib/libprotobuf.a -o $OUT/fuzz_lpm
+)
